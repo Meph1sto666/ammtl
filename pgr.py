69a662dedfa1717421d252b30900aaebd12c52c2
Meph1sto666 <101014274+Meph1sto666@users.noreply.github.com>
2023-09-15 22:20:54 +0200

    finished bounding box ui for #training

--- pgr.py
+++ pgr.py
@@ -1,47 +1,68 @@
-from lib.types.bubble import Bubble
-from lib.types.page import Page
+import json
+from lib.types.bubble import Bubble # type: ignore
+from lib.types.page import Page # type: ignore
 import tkinter as tk
 from PIL import ImageTk, Image
 
 class PagePreview:
-	def __init__(self, root:tk.Tk) -> None:
+	def __init__(self, root:tk.Tk, page:str) -> None:
+		self.page:str = page
 		self.rectangles:list[list[int|tuple[int,int,int,int]|bool]] = []
-
-		image:Image = Image.open("tests/4.jpg") # type: ignore
+		self.savedCoords:list[tuple[int, ...]] = []
+		self.root = root
+		image:Image = Image.open(page) # type: ignore
 		self.image_tk = ImageTk.PhotoImage(image) # type: ignore
 		self.canvas = tk.Canvas(root, width=self.image_tk.width(), height=self.image_tk.height(), bg="black")
-		self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-		
+		self.canvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 		self.canvas.configure(scrollregion=self.canvas.bbox("all"))
-		self.canvas.create_image(0, 0, anchor='nw', image=self.image_tk) # type: ignore
-		self.canvas.bind('<Motion>', self.update) # type:ignore
-		self.canvas.bind("<Button-1>", self.handleLeft) # type:ignore
-		self.canvas.bind_all("<Control-z>" ,self.delLast) # type:ignore
 
-	def update(self, event:tk.Event) -> None:
+		scrollbar = tk.Scrollbar(self.root, orient=tk.VERTICAL, command=self.canvas.yview) # type: ignore
+		self.canvas.configure(yscrollcommand=scrollbar.set)
+
+		# ===bindings===
+		self.canvas.create_image(0, 0, anchor='nw', image=self.image_tk) # type: ignore
+		self.canvas.bind('<Motion>', self.update) # type: ignore
+		self.canvas.bind("<Button-1>", self.handleLeft) # type: ignore
+		self.canvas.bind_all("<Control-z>", self.delLast) # type: ignore
+		self.canvas.bind_all("<Control-s>", self.saveRecs) # type: ignore
+		self.canvas.bind("<MouseWheel>", self.scroll_canvas) # type: ignore
+
+	def scroll_canvas(self, event:tk.Event): # type: ignore
+		if (int(event.state)-8) == 1:
+			self.canvas.xview_scroll(int(-1 * (event.delta / 120)), "units")  # Horizontal scroll
+		else:
+			self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")  # Vertical scroll
+
+	def update(self, event:tk.Event) -> None: # type: ignore
 		if len(self.rectangles) < 1: return
 		if self.rectangles[-1][1]: return
-		x1:int = int(self.rectangles[-1][2][0]) if self.rectangles[-1][2][0] < event.x else event.x
-		y1:int = int(self.rectangles[-1][2][1]) if self.rectangles[-1][2][1] < event.y else event.y
-		x2:int = int(self.rectangles[-1][2][0]) if self.rectangles[-1][2][0] > event.x else event.x
-		y2:int = int(self.rectangles[-1][2][1]) if self.rectangles[-1][2][1] > event.y else event.y
-		self.canvas.coords(self.rectangles[-1][0], x1, y1, x2, y2)
+		ex = event.widget.canvasx(event.x) # type: ignore
+		ey = event.widget.canvasy(event.y) # type: ignore
+		x1:int = (int(self.rectangles[-1][2][0]) if self.rectangles[-1][2][0] < ex else ex) # type: ignore
+		y1:int = (int(self.rectangles[-1][2][1]) if self.rectangles[-1][2][1] < ey else ey) # type: ignore
+		x2:int = (int(self.rectangles[-1][2][0]) if self.rectangles[-1][2][0] > ex else ex) # type: ignore
+		y2:int = (int(self.rectangles[-1][2][1]) if self.rectangles[-1][2][1] > ey else ey) # type: ignore
+		self.canvas.coords(self.rectangles[-1][0], x1, y1, x2, y2) # type: ignore
 
-	def handleLeft(self, event:tk.Event) -> None:
+	def handleLeft(self, event:tk.Event) -> None: # type: ignore
 		if not self.rectangles[-1][1] if len(self.rectangles) > 0 else False:
 			self.rectangles[-1][1] = True
 			return
-		rec:int = self.canvas.create_rectangle(event.x, event.y, event.x+50, event.y+50, outline="red")
-		self.rectangles.append([rec, False, (event.x, event.y, event.x, event.y)])
+		ex = event.widget.canvasx(event.x) # type: ignore
+		ey = event.widget.canvasy(event.y) # type: ignore
+		rec:int = self.canvas.create_rectangle(ex, ey, ex, ey, outline="red") # type: ignore
+		self.rectangles.append([rec, False, (ex, ey, ex, ey)])
 
-	def delLast(self, event) -> None:
+	def delLast(self, event) -> None: # type: ignore
 		if len(self.rectangles) < 1: return
-		self.canvas.delete(self.rectangles[-1][0])
+		self.canvas.delete(self.rectangles[-1][0]) # type: ignore
 		self.rectangles.pop()
 
+	def saveRecs(self, event:tk.Event) -> None: # type: ignore
+		self.savedCoords = [tuple(self.canvas.coords(r[0])) for r in self.rectangles] # type: ignore
+		json.dump(self.savedCoords, open(f"./presets/traindata/{self.page.split('/')[-1].split('.')[0]}.json", "w", encoding="utf-8"))
+		
 
 root:tk.Tk = tk.Tk()
-exit = tk.Button(root, text="Exit", command=root.destroy)
-exit.pack()
-pp = PagePreview(root)
+pp = PagePreview(root, "./tests/007.jpg")
 root.mainloop()
